462239ba6b91867bc0792a95c94f5ca5d15bdcb9
Shahd-404 <sa1200295@gmail.com>
2024-12-25 17:25:57 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+import sklearn
+import helper 
+import pickle
+import nltk
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+model = pickle.load(open('models/model.pkl' , 'rb'))
+vectorizer = pickle.load(open('models/vectorizer.pkl' , 'rb'))
+
+st.title ('Sentiment Analysis App using ML')
+
+
+text = st.text_input('please, enter your review')
+
+token = helper.preprocessing_step(text)
+
+vectorized_data = vectorizer.transform([token])
+
+prediction = model.predict(vectorized_data)
+
+state = st.button('predict') 
+if state : 
+    st.text(prediction)
+
+
